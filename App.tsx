064f87dcff6b0588bd0fb28813c3e94b6400bbbb
vniciuslobo <viniciusd.lobo@gmail.com>
2026-02-27 20:35:46 -0300

    sim kaique

--- App.tsx
+++ App.tsx
@@ -1,20 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, ScrollView, Image } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView style={styles.container}>
+      <Text style={styles.texto}>um gato</Text>
+      <Image source={require('./assets/gato2.png')} style={styles.gato} resizeMode='cover'/>
+      <Text style={styles.texto2}>apenas um gato</Text>
+      <StatusBar style="light" animated/>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#1249be',
+    alignSelf: 'center'
   },
+  texto: {
+    color: '#000000',
+    fontWeight: 500,
+    fontSize: 110,
+    textAlignVertical: 'center'
+  },
+  texto2: {
+    color: '#000000',
+    fontWeight: 500,
+    fontSize: 30,
+    textAlign: 'center',
+  },
+  gato: {
+    height:200,
+    width:600,
+  }
 });
