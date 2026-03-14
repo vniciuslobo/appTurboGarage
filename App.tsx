2e77546bf59efe0f1e0b4e0aa457bbc18fc110ae
vniciuslobo <viniciusd.lobo@gmail.com>
2026-03-13 22:08:25 -0300

    é isso ai

--- App.tsx
+++ App.tsx
@@ -2,45 +2,40 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, ScrollView, Image } from 'react-native';
 import { VideoView, useVideoPlayer} from 'expo-video';
 
-export default function App() {
+// icones menu
+import AntDesign from '@expo/vector-icons/AntDesign';
 
-  const player = useVideoPlayer ('http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/VolkswagenGTIReview.mp4', player => {player.loop=true, player.play()})
+// menu sobre
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import telaSobre from './Telas/Sobre';
 
-  return (
-    <ScrollView style={styles.container}>
-      <Text style={styles.texto}>um gato</Text>
-      <Image source={require('./assets/gato2.png')} style={styles.gato} resizeMode='cover'/>
-      <VideoView player={player} style={styles.video} allowsFullscreen allowsPictureInPicture/>
-      <Text style={styles.texto2}>apenas um gato</Text>
-      <StatusBar style="light" animated/>
-    </ScrollView>
-  );
+const Tab = createBottomTabNavigator();
+
+function Menu() {
+  return <Tab.Navigator screenOptions={({route})=>({
+    tabBarIcon:({focused, color, size})=>{
+      let iconName: any;
+
+      if(route.name==="Sobre"){
+        iconName = focused?'baidu':'baidu-outline';
+      }
+      return <AntDesign name={iconName} size={size} color={color} />
+    },
+    headerShown: false,
+    tabBarActiveTintColor: 'blue',
+    tabBarInactiveTintColor: 'gray',
+    tabBarInactiveBackgroundColor: 'black'
+  })}>
+    <Tab.Screen name="Sobre" component={telaSobre}/>
+    <Tab.Screen name="Sobre2" component={telaSobre}/>
+    <Tab.Screen name="Sobre3" component={telaSobre}/>
+    <Tab.Screen name="Sobre4" component={telaSobre}/>
+  </Tab.Navigator>
+};
+
+export default function App(){
+  return <NavigationContainer>
+    <Menu/>
+  </NavigationContainer>
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#1249be',
-    alignSelf: 'center'
-  },
-  texto: {
-    color: '#000000',
-    fontWeight: 500,
-    fontSize: 110,
-    textAlignVertical: 'center'
-  },
-  texto2: {
-    color: '#9dff00',
-    fontWeight: 500,
-    fontSize: 30,
-    textAlign: 'center',
-  },
-  gato: {
-    height:200,
-    width:600,
-  },
-  video: {
-    width: 300,
-    height: 400
-  }
-});
---
+++ Telas/Sobre.tsx
@@ -0,0 +1,53 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, ScrollView, Image } from 'react-native';
+import { VideoView, useVideoPlayer} from 'expo-video';
+
+// menu
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+export default function Sobre() {
+  
+  // indica video e roda ele em loop
+  const player = useVideoPlayer ('http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/VolkswagenGTIReview.mp4', player => {player.loop=true, player.play()})
+
+  return (
+    <ScrollView style={styles.container}>
+      <Text style={styles.texto}>um gato</Text>
+      <Image source={require('../assets/gato2.png')} style={styles.gato} resizeMode='cover'/>
+      <VideoView player={player} style={styles.video} allowsFullscreen allowsPictureInPicture/>
+      <Text style={styles.texto2}>apenas um gato</Text>
+      <StatusBar style="light" animated/>
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#1249be',
+    alignSelf: 'center',
+    flexDirection: 'column'
+  },
+  texto: {
+    color: '#000000',
+    fontWeight: 500,
+    fontSize: 110,
+    textAlignVertical: 'center'
+  },
+  texto2: {
+    color: '#9dff00',
+    fontWeight: 500,
+    fontSize: 30,
+    textAlign: 'center',
+  },
+  gato: {
+    height:200,
+    width:600,
+  },
+  video: {
+    width: 300,
+    height: 400
+  }
+});
