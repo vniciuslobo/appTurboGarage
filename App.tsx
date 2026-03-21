5faea6e30d772ea335909115df24dc50f4fd094a
vniciuslobo <viniciusd.lobo@gmail.com>
2026-03-20 21:57:00 -0300

    atualizações

--- App.tsx
+++ App.tsx
@@ -3,7 +3,8 @@ import { StyleSheet, Text, ScrollView, Image } from 'react-native';
 import { VideoView, useVideoPlayer} from 'expo-video';
 
 // icones menu
-import AntDesign from '@expo/vector-icons/AntDesign';
+// import AntDesign from '@expo/vector-icons/AntDesign';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 // menu sobre
 import { NavigationContainer } from '@react-navigation/native';
@@ -17,20 +18,24 @@ function Menu() {
     tabBarIcon:({focused, color, size})=>{
       let iconName: any;
 
-      if(route.name==="Sobre"){
-        iconName = focused?'baidu':'baidu-outline';
+      if(route.name==="Happy"){
+        iconName = focused?'happy':'happy-outline';
+      }else if(route.name==="Info"){
+        iconName = focused?'information-circle':'information-circle-outline';
+      }else if(route.name==="Options"){
+        iconName = focused?'apps':'apps-outline';
       }
-      return <AntDesign name={iconName} size={size} color={color} />
+      return <Ionicons name={iconName} size={size} color={color} />
+      // return <AntDesign name={iconName} size={size} color={color} />
     },
     headerShown: false,
     tabBarActiveTintColor: 'blue',
     tabBarInactiveTintColor: 'gray',
     tabBarInactiveBackgroundColor: 'black'
   })}>
-    <Tab.Screen name="Sobre" component={telaSobre}/>
-    <Tab.Screen name="Sobre2" component={telaSobre}/>
-    <Tab.Screen name="Sobre3" component={telaSobre}/>
-    <Tab.Screen name="Sobre4" component={telaSobre}/>
+    <Tab.Screen name="Happy" component={telaSobre}/>
+    <Tab.Screen name="Info" component={telaSobre}/>
+    <Tab.Screen name="Options" component={telaSobre}/>
   </Tab.Navigator>
 };
 
